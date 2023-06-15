48258af3e1b43c665171285d5d7666896f3eb10e
SamBobBarnes <samuelrbarnes@outlook.com>
2023-06-14 22:14:11 -0600

    basic grid of heroes

--- src/mad-lib/src/app/app.module.ts
+++ src/mad-lib/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeroListComponent } from './heroList/hero-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, HeroListComponent],
@@ -15,6 +17,8 @@ import { MatTabsModule } from '@angular/material/tabs';
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     BrowserAnimationsModule,
     MatTabsModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
