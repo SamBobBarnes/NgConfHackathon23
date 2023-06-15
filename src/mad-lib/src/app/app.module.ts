93371b2781638e47b2eb6ee5e7009c6e4f64f472
SamBobBarnes <samuelrbarnes@outlook.com>
2023-06-14 21:22:35 -0600

    wip

--- src/mad-lib/src/app/app.module.ts
+++ src/mad-lib/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { HeroListComponent } from './components/hero-list/hero-list.component';
+import { HeroListComponent } from './heroList/hero-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, HeroListComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/mad-lib/src/app/app.routes.ts
+++ src/mad-lib/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { HeroListComponent } from './heroList/hero-list.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HeroListComponent,
+  },
+];
